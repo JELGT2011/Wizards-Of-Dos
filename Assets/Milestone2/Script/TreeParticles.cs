8b9ac996ececfc183aab67d80e39009b5f28e616
jcarrillo9 <jcarrillo9@gatech.edu>
2015-10-16 14:24:11 -0400

    Milestone 2

---
+++ Assets/Milestone2/Script/TreeParticles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeParticles : MonoBehaviour {
+    public GameObject snow;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Instantiate(snow, transform.position, Quaternion.identity);
+    }
+}
