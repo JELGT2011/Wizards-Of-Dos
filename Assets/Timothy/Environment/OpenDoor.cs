effece622d2245f9b6f22f1d739bce44912f2f6d
Timothy <timothybstorm@gmail.com>
2015-10-17 05:11:15 -0400

    Environment

---
+++ Assets/Timothy/Environment/OpenDoor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	Rigidbody DRB;
+
+	[SerializeField]
+	private float direction = 1;
+	[SerializeField]
+	private float forcePower = 1150;
+	private float distance = 0;
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		DRB = GetComponent<Rigidbody>();
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		triggerDoor();
+
+	}
+
+	void triggerDoor()
+	{
+		distance = Vector3.Distance(player.transform.position,this.transform.position);
+
+
+		if(Input.GetKeyDown(KeyCode.O) && distance <= 3)
+		{
+			DRB.AddForce(transform.forward * forcePower * direction, ForceMode.Impulse);
+			print("open");
+		}
+	}
+}
