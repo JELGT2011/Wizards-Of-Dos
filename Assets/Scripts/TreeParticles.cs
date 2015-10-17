ced8e817b99d4724b555f22690020333da273bb9
jcarrillo9 <jcarrillo9@gatech.edu>
2015-10-16 20:04:10 -0400

    Milestone 2 File moving

---
+++ Assets/Scripts/TreeParticles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeParticles : MonoBehaviour {
+    public GameObject snow;
+    Vector3 trans;
+    Object temp;
+    void OnCollisionEnter(Collision col)
+    {
+        trans = transform.position;
+        trans.y += 5;
+        temp = Instantiate(snow, trans, Quaternion.identity);
+        Destroy(temp, 5f);
+    }
+}
