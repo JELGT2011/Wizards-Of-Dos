a3e6df624185cc3d83011db96ee002cd59b2f9b3
zchen39 <ziranchen@yahoo.com>
2015-10-16 20:45:21 -0400

    Added scene switching

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Debug.Log ("called");
+			Application.LoadLevel(0);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			Application.LoadLevel(1);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			Application.LoadLevel(2);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			Application.LoadLevel(3);
+		}
+	}
+}
