489f33faac9139a3524d4303ce62e466ed8e8fb0
timothybstorm <timothybstorm@gmail.com>
2015-11-05 22:38:38 -0500

    Agressive tree Finished

---
+++ Assets/AI/Actions/Ragdoll.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class Ragdoll : RAINAction
+{
+	RagdollController rdc; 
+	
+	public Ragdoll()
+	{
+		actionName = "Ragdoll";
+	}
+
+    public override void Start(RAIN.Core.AI ai)
+    {
+		rdc = ai.Body.GetComponent<RagdollController> ();
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+
+		if (ai.WorkingMemory.GetItem<float>("AI_Health") <= 0) 
+		{
+			rdc.triggerRagdoll();
+		}
+
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
