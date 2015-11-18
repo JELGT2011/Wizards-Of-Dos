dd7d87899433d219ca8e968051ed38868fe320db
Jason Libbey <jelgt2011@gmail.com>
2015-11-18 14:20:07 -0500

    clean up and reorganizing

---
+++ Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+	[SerializeField]
+	protected ParticleSystem[] _particleSystem;
+	public ParticleSystem[] ParticleSystem
+	{
+		get { return _particleSystem; }
+		set { _particleSystem = value; }
+	}
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+	
+	}
+}
