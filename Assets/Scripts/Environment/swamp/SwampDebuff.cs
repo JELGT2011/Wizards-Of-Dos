668c7bb7c340b98a2f4e319a399d760dd6fed708
zchen39 <ziranchen@yahoo.com>
2015-12-04 23:19:19 -0500

    fixed minor collision issue

--- Assets/Scripts/Environment/swamp/SwampDebuff.cs
+++ Assets/Scripts/Environment/swamp/SwampDebuff.cs
@@ -24,6 +24,8 @@ public class SwampDebuff : MonoBehaviour {
 		}
 	}
 	void OnTriggerExit(Collider collider){
+		if (!collider.gameObject.tag.Equals("Player1")&& !collider.gameObject.tag.Equals("Player2"))
+			return;
 		CharacterStats stats = collider.gameObject.GetComponent<CharacterStats> (); //is this triggered multiple times?
 		if (stats.HasBuff("SwampDamage")) {
 			stats.RemoveBuff("SwampDamage");
